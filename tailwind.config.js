62f77ea582d15aceea927fd23fa3a4499c7cdbe0
Mokut001 <mokutoton01@gmail.com>
2026-02-16 00:21:20 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        aura: {
+          light: '#e0f2fe',
+          DEFAULT: '#0ea5e9',
+          dark: '#0369a1',
+        },
+        hospital: '#1e293b'
+      }
+    }
+  },
+  plugins: [],
+}
